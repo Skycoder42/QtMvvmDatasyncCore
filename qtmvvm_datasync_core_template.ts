bb56bf15db0d418aa0a7581290cc671afe4b657c
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-24 14:42:58 +0200

    added translations

---
+++ qtmvvm_datasync_core_template.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DatasyncControl</name>
+    <message>
+        <location filename="datasynccontrol.cpp" line="49"/>
+        <source>Loading…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="51"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="53"/>
+        <source>Synchronizing…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="55"/>
+        <source>Synchronized</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="57"/>
+        <source>Synced with errors!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="80"/>
+        <source>Error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="119"/>
+        <location filename="datasynccontrol.cpp" line="124"/>
+        <source>User data import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="119"/>
+        <source>Import successfully completed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="124"/>
+        <source>Import failed with error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="141"/>
+        <location filename="datasynccontrol.cpp" line="171"/>
+        <location filename="datasynccontrol.cpp" line="174"/>
+        <source>Reset Identity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="142"/>
+        <source>Do you want to keep your local data, or delete everything?&lt;br/&gt;&lt;b&gt;Warning:&lt;/b&gt; Deleting your data cannot be undone. If no other device is connected to this identity, the data is lost permanently!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="146"/>
+        <source>Delete Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="147"/>
+        <source>Keep Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="148"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="172"/>
+        <source>Identity successfully resetted!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="datasynccontrol.cpp" line="175"/>
+        <source>Failed to reset Identity with error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserDataExchangeControl</name>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="56"/>
+        <source>Send user data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="57"/>
+        <source>Enter a key to protect your data with before sending (optional):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="82"/>
+        <source>Port binding failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="82"/>
+        <source>Failed to bind to selected port with error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="175"/>
+        <source>&lt;Unnamed&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="199"/>
+        <location filename="userdataexchangecontrol.cpp" line="202"/>
+        <source>User data import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="199"/>
+        <source>Import successfully completed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="202"/>
+        <source>Import failed with error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="206"/>
+        <location filename="userdataexchangecontrol.cpp" line="225"/>
+        <source>Receive user data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="207"/>
+        <source>You received user data! If you want to import it, enter the key to decrypt the received data:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="223"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="userdataexchangecontrol.cpp" line="226"/>
+        <source>Do you want to import the received user data?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
